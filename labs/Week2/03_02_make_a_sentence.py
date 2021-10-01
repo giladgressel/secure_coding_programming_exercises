1f4fffbf14539e90bab0094806e793e4bb2b3343
Gilad Gressel <giladgressel@gmail.com>
2021-10-01 19:29:25 +0530

    week2 labs first pass

---
+++ labs/Week2/03_02_make_a_sentence.py
@@ -0,0 +1,8 @@
+#Place a curly bracket "{}" in the appropriate place and fill name, branch, college_name inside .format()'s parenthesis so that 
+# "Hello, name. Welcome to  deptname in collegname" is printed.
+
+name =""
+dept = ""
+college_name = ""
+
+
---
+++ labs/Week2/05_02_three_way_logic.py
@@ -0,0 +1,9 @@
+#Logical_operators_4
+
+# Initialize variable a to true, b to false and c to true
+# Print the value of a and b or c
+
+#Assign c to false and print the value of a and b or c
+
+
+# Understand the difference in each scenario
