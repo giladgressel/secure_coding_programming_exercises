4f6d74ac87a5547beb51b713ba5ee86bd3592b2a
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2023-08-25 14:22:05 +0530

    exercises for students

---
+++ supplementary_exercises/hangman.py
@@ -0,0 +1,11 @@
+# Hard-code a word that needs to be guessed in the script
+# Print an explanation to the user
+# Display the word as a sequence of blanks, e.g. "_ _ _ _ _" for "hello"
+# Ask for user input
+# Allow only single-character alphabetic input
+# Create a counter for how many tries a user has
+# Keep asking them for their guess until they won or lost
+# When they find a correct character, display the blank with the word
+#   filled in, e.g.: "_ e _ _ _" if they guessed "e" from "hello"
+# Display a winning message and the full word if they win
+# Display a losing message and quit the game if they don't make it
