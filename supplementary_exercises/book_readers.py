00f6eab9fdc5e9c17a95868b7ce68b797dfbe07e
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2023-08-25 14:19:28 +0530

    exercises for students

---
+++ supplementary_exercises/book_readers.py
@@ -0,0 +1,9 @@
+# This folder contains another folder called `books/` that contains three text files
+# of books from Project Gutenberg:
+# 1. war_and_peace.txt
+# 2. pride_and_prejudice.txt
+# 3. crime_and_punishment.txt
+#
+# 1) Open `war_and_peace.txt`, read the whole file content and store it in a variable
+# 2) Open `crime_and_punishment.txt` and overwrite the whole content with the last two words of the text of `war_and_peace.txt`
+# 3) Loop over all three files and print out only the first word of each.
---
+++ supplementary_exercises/csv_practice.py
@@ -0,0 +1,4 @@
+#Open the csv_practice_users.csv doc
+#Retrieve all names that start with an S
+#Add the ages of all the users print the total
+#Retrieve the phone number of the user Jay 
